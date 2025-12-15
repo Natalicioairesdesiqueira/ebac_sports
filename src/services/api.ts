3ede81024c95a98d7d59a1594e07ca314431f793
sique <siqueira.natalicio08@gmail.com>
2025-12-15 10:16:40 -0300

    exercicio para ebac

---
+++ src/services/api.ts
@@ -0,0 +1,15 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { Produto } from '../App'
+
+export const api = createApi({
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://api-ebac.vercel.app/api'
+  }),
+  endpoints: (builder) => ({
+    getProdutos: builder.query<Produto[], void>({
+      query: () => '/ebac_sports'
+    })
+  })
+})
+
+export const { useGetProdutosQuery } = api
